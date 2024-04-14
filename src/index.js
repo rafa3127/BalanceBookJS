15a100d628cc430f2067538002c9c977f63607dd
Rafael Correa <rafaelc3127@PDDMNB966.local>
2024-04-14 16:41:34 -0400

    feat(versions): added webpack compilation

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// Importing classes using ES Modules syntax
+import Account from './classes/accounts/Account.js';
+import Asset from './classes/accounts/Asset.js';
+import Liability from './classes/accounts/Liability.js';
+import Equity from './classes/accounts/Equity.js';
+import Income from './classes/accounts/Income.js';
+import Expense from './classes/accounts/Expense.js';
+import JournalEntry from './classes/transactions/JournalEntry.js';
+
+export {
+    Account,
+    Asset,
+    Liability,
+    Equity,
+    Income,
+    Expense,
+    JournalEntry
+};
